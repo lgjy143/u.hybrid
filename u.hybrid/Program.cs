352d3bc9484edcf184c767449a7f0fb836b2a474
U724 <lgjy143@163.com>
2018-02-07 14:45:30 +0800

    添加项目文件。

---
+++ u.hybrid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace u.hybrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
